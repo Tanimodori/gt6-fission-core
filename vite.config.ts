db0cbf210c3a7b5f63b5821993447d0375d0a935
Tanimodori <unknowner2014@gmail.com>
2022-08-20 08:55:21 +0800

    chore: add vite config and build settings

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="node" />
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      formats: ['es', 'cjs'],
+      fileName: '[format]/index',
+    },
+  },
+});

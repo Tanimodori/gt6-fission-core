3d12eb46c33aa080696b2b10c9fd19524e78a851
Tanimodori <unknowner2014@gmail.com>
2022-08-20 15:00:16 +0800

    docs: simulation docs

--- src/simulation/simulator.ts
+++ src/simulation/simulator.ts
@@ -1,10 +1,22 @@
 import { Config, CellInput, Rod, RodType, Cell } from 'src/types';
 
+/**
+ * Get the rod type data by name.
+ * @param type the type name of rod
+ * @param types a list of available rod types if provided
+ * @returns the rod type with matched name
+ */
 function getRodType(type: string, types?: RodType[]) {
   // TODO
   return -1;
 }
 
+/**
+ * Get the default position of rod by index.
+ * Supports cell with up to 2x2 rod size.
+ * @param index the index of the rod in its cell
+ * @returns the corresponding x and y data
+ */
 function getDefaultPos(index: number) {
   if (index < 0 || index >= 4) {
     throw new Error(`Default pos of rod[${index}]is out of range [0,3]`);
@@ -14,6 +26,11 @@ function getDefaultPos(index: number) {
   return { x, y };
 }
 
+/**
+ * Sanitize cell data before simulation
+ * @param cellsIn input cell data
+ * @returns cell data sanitized
+ */
 function initCells(cellsIn: CellInput[]) {
   const cellsOut: Cell[] = [];
   for (const cellIn of cellsIn) {
@@ -30,6 +47,10 @@ function initCells(cellsIn: CellInput[]) {
   return cellsOut;
 }
 
+/**
+ * Run the simulation.
+ * @param config simulation configuration
+ */
 export function simulate(config: Config) {
   const cells = initCells(config.cells);
   const rods = cells.flatMap((x) => x.rods);

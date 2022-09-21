412a90008ba509b3cf1aa7bf301beedc8d3786ef
Tanimodori <unknowner2014@gmail.com>
2022-09-21 21:03:30 +0800

    test: rodTypeRegistry

--- test/rodTypeRegistry.spec.ts
+++ test/rodTypeRegistry.spec.ts
@@ -11,12 +11,18 @@ describe('RodTypeRegistry.defaultRegistry', () => {
   it('Should return same registry', () => {
     expect(RodTypeRegistry.defaultRegistry).toBe(RodTypeRegistry.defaultRegistry);
   });
+});
 
+describe('RodTypeRegistry.get', () => {
   it('Can query rod types', () => {
     const registry = RodTypeRegistry.defaultRegistry;
     const rodType = registry.types[0];
     expect(registry.get(rodType.fullname)).toBe(rodType);
     expect(registry.get(rodType.id)).toBe(rodType);
+  });
+
+  it('Returns null if not found', () => {
+    const registry = RodTypeRegistry.defaultRegistry;
     expect(registry.get('NotExistRodType')).toBe(null);
     expect(registry.get(-1)).toBe(null);
   });
